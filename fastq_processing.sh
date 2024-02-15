9154c729a786b26eae4e56d44ced00ea4f55a9cb
Kacper Kaszuba <117150358+KacperKaszuba0608@users.noreply.github.com>
2024-02-15 12:19:45 +0100

    Update fastq_processing.sh

--- fastq_processing.sh
+++ fastq_processing.sh
@@ -5,18 +5,18 @@ read -p 'Enter the number of BioProject: ' no_project
 
 if [[ -n "$no_project" ]]; then
 
-        # Krok 1 - pobieranie informacji o projeckie podanym przez użytkownika
-        esearch -db sra -query "$no_project" | efetch -format runinfo -mode xml | xtract -pattern SraRunInfo -element R>
+        # Step 1 - download information about BioProject entered by the user
+        esearch -db sra -query "$no_project" | efetch -format runinfo -mode xml | xtract -pattern SraRunInfo -element Run > runinfo.txt
 
-        ## obróbka
+        ## SRR numers processing
 
         tr "\t" "\n" < runinfo.txt > SRR_list.txt
 
-        ## Przygotowanie folderów do podziału na pair_end and single_end
+        ## Preparing folders for pair_end and single_end splitting
 
         mkdir pair_end single_end
 
-        # Krok 2
+        # Step 2
         read -p 'Enter the number of readings for the fastq-dump programme: ' no_reads
 
         while IFS= read -r AR; do
@@ -32,7 +32,7 @@ if [[ -n "$no_project" ]]; then
 
         done < SRR_list.txt
 
-        # Krok 3 kontrola jakości
+        # Step 3 quality control
 
         mkdir post_trimm pre_trimm
 
@@ -40,7 +40,7 @@ if [[ -n "$no_project" ]]; then
 
         fastqc single_end/* -o pre_trimm
 
-        # Krok 4 trimming
+        # Step 4 trimming
 
         mkdir trimmed_pair_end trimmed_single_end
 
@@ -56,7 +56,7 @@ if [[ -n "$no_project" ]]; then
                 filename2="${filename2%.*}";
 
                 echo Trimming of $filename1 and $filename2 ...;
-               java -jar /bioapp/Trimmomatic-0.39/trimmomatic-0.39.jar PE ${seq_1_list[i]} ${seq_2_list[i]} trimmed_$filename1.fq unpaired_$filename1.fq trimmed_$filename2.fq unpaired_$filena>
+               java -jar /bioapp/Trimmomatic-0.39/trimmomatic-0.39.jar PE ${seq_1_list[i]} ${seq_2_list[i]} trimmed_$filename1.fq unpaired_$filename1.fq trimmed_$filename2.fq unpaired_$filename2.fq5 SLIDINGWINDOW:4:30
 
                 mv trimmed_*.fq ./trimmed_pair_end;
                 rm ./unpaired*;
@@ -76,28 +76,28 @@ if [[ -n "$no_project" ]]; then
                 mv trimmed_*.fq ./trimmed_single_end;
         done
 
-        # Krok 5 - quality control of trimmed files
+        # Step 5 - quality control of trimmed files
 
         fastqc ./trimmed_pair_end/* -o post_trimm
 
         fastqc ./trimmed_single_end/* -o post_trimm
 
-        # Krok 6 - mapowanie z genomem hg19
+        # Step 6 - mapping with hg19 genom
 
-        # pobieranie pliku z genomem referencyjnym hg19
+        # download the hg19 reference genome file
         wget https://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.fa.gz
 
-        # rozpakowywanie pliku hg19
+        # unzip reference genome file
         gzip -dk hg19.fa.gz
 
-        # Tworzenie indeksu dla programu hisat2 na podsatwie genomu referencyjnego
+        # Creation of an index for hisat2 based on a reference genome
         hisat2-build -p 4 -f hg19.fa hg19
 
         mkdir ./refs
 
         mv hg19.*.ht2 hg19.fa.gz hg.19fa ./refs
 
-        # mapowanie sekwencji pair end do genomu referencyjnego
+        # Mapping of paired-end sequences to a reference genome
 
         mkdir sam_files
 
@@ -114,7 +114,7 @@ if [[ -n "$no_project" ]]; then
 
         mv *.sam ./sam_files
 
-        # mapowanie sekwencji single end do genomu referencyjnego
+        # Mapping of single-end sequences to a reference genome
         single_seq=$PWD/trimmed_single_end/*.fq
 
         for file in $single_seq; do
@@ -126,7 +126,7 @@ if [[ -n "$no_project" ]]; then
 
         mv *.sam ./sam_files
 
-        # Krok 7 - konwersja plików sam do bam
+        # Step 7 - Conversion of SAM files to BAM
 
         mkdir bam_files
 
@@ -139,16 +139,16 @@ if [[ -n "$no_project" ]]; then
                 samtools view -Sb -@ 4 $fsam > $filename.bam | mv $filename.bam ./bam_files;
         done
 
-        # Krok 8 - zliczanie odczytów
+        # Step 8 - Counting readings
 
-        # Pobieranie pliku gtf dla odpowiedniego genomu
+        # Download the gtf file for the corresponding genome
         wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.refGene.gtf.gz
 
-        # Rozpakowanie i zmiana nazwy
-        gzip -dk hg19.refGene.gtf.gz | mv hg19.reFGene.gtf hg19.gtf | mv ./hg19 ../refs
+        # unzip, change name and move to refs folder
+        gzip -dk hg19.refGene.gtf.gz | mv hg19.reFGene.gtf hg19.gtf | mv ./hg19.gtf ../refs
 
         featureCounts -a ../refs/hg19.gtf -g gene_name -o counts.txt ./bam_files/*.bam
 
 else
-        echo "Nie podano numeru projektu";
+        echo "BioProject number not provided";
 fi;
